d49189799a0678ce3a88d53deda2a97ff071ddc3
benjaminha14 <benha@Benjamins-MacBook-Pro.local>
2016-06-11 14:53:02 -0700

    Added cocoapods

---
+++ Poochie/Pods/Target Support Files/Pods-Poochie/Pods-Poochie-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Poochie : NSObject
+@end
+@implementation PodsDummy_Pods_Poochie
+@end
---
+++ Poochie/Pods/Target Support Files/Pods-PoochieTests/Pods-PoochieTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PoochieTests : NSObject
+@end
+@implementation PodsDummy_Pods_PoochieTests
+@end
